adc9a0e788e27b408e1cf3542dcd1f7c9f2d39e6
not-louis-239 <louis.boulton@icloud.com>
2026-04-15 13:48:19 +0800

    Add bytecode to run_brainfuck in preparation to optimise the interpreter

--- src/brainfuck/fundamentals/run_brainfuck.py
+++ src/brainfuck/fundamentals/run_brainfuck.py
@@ -1,7 +1,67 @@
+from enum import StrEnum
 import sys
 
 from .exceptions import BFSegmentationFault, BFSyntaxError, BFInterrupt
 from .keywords import BrainfuckKeywords
+
+class BFBytecodeType(StrEnum):
+    VAL_CHANGE = 'VAL_CHANGE'
+    PTR_CHANGE = 'PTR_CHANGE'
+    STDOUT = 'STDOUT'
+    STDIN = 'STDIN'
+    LOOP_START = 'LOOP_START'
+    LOOP_END = 'LOOP_END'
+
+class BFBytecodeToken:
+    def __init__(self, typ: BFBytecodeType, val: int | None = None) -> None:
+        self.typ = typ
+        self.val = val
+
+def convert_bf_to_bytecode(code: str) -> list[BFBytecodeToken]:
+    """Converts Brainfuck source to optimized bytecode tokens,
+    condensing chained operators into single tokens."""
+    bytecode: list[BFBytecodeToken] = []
+    ist = 0
+    code_len = len(code)
+
+    while ist < code_len:
+        char = code[ist]
+
+        # Value changes
+        if char in (BrainfuckKeywords.VAL_INC, BrainfuckKeywords.VAL_DEC):
+            chain_value = 0
+            while ist < code_len and code[ist] in (BrainfuckKeywords.VAL_INC, BrainfuckKeywords.VAL_DEC):
+                chain_value += 1 if code[ist] == BrainfuckKeywords.VAL_INC else -1
+                ist += 1
+            if chain_value != 0:
+                chain_value = (chain_value + 128) % 256 - 128
+                bytecode.append(BFBytecodeToken(BFBytecodeType.VAL_CHANGE, chain_value))
+            continue # Skip the ist += 1 at the bottom because the inner loop advanced it
+
+        # Pointer changes
+        elif char in (BrainfuckKeywords.PTR_INC, BrainfuckKeywords.PTR_DEC):
+            chain_value = 0
+            while ist < code_len and code[ist] in (BrainfuckKeywords.PTR_INC, BrainfuckKeywords.PTR_DEC):
+                chain_value += 1 if code[ist] == BrainfuckKeywords.PTR_INC else -1
+                ist += 1
+            if chain_value != 0:
+                chain_value = (chain_value + 128) % 256 - 128
+                bytecode.append(BFBytecodeToken(BFBytecodeType.PTR_CHANGE, chain_value))
+            continue
+
+        # Static tokens
+        elif char == BrainfuckKeywords.STDOUT:
+            bytecode.append(BFBytecodeToken(BFBytecodeType.STDOUT))
+        elif char == BrainfuckKeywords.STDIN:
+            bytecode.append(BFBytecodeToken(BFBytecodeType.STDIN))
+        elif char == BrainfuckKeywords.LOOP_START:
+            bytecode.append(BFBytecodeToken(BFBytecodeType.LOOP_START))
+        elif char == BrainfuckKeywords.LOOP_END:
+            bytecode.append(BFBytecodeToken(BFBytecodeType.LOOP_END))
+
+        ist += 1
+
+    return bytecode
 
 def validate_brainfuck(code: str) -> None:
     stack: list[int] = []
