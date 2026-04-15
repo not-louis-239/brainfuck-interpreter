75f6f64d691614addbf4217cc79332f82f190171
not-louis-239 <louis.boulton@icloud.com>
2026-04-15 13:35:33 +0800

    Rename variables in run_brainfuck for conciseness

--- src/brainfuck/fundamentals/run_brainfuck.py
+++ src/brainfuck/fundamentals/run_brainfuck.py
@@ -28,44 +28,44 @@ def validate_brainfuck(code: str) -> None:
         )
 
 def run_brainfuck(code: str, *, memsize: int, wrap: bool = False):
-    membuf: list[int] = [0] * memsize
-    pointer_idx = 0
-    instruct_idx = 0
+    mem: list[int] = [0] * memsize
+    ptr = 0
+    ist = 0
     prog_len = len(code)
 
-    while instruct_idx < prog_len:
-        char = code[instruct_idx]
+    while ist < prog_len:
+        char = code[ist]
 
         if char == BrainfuckKeywords.VAL_INC:
-            membuf[pointer_idx] = (membuf[pointer_idx] + 1) % 256
+            mem[ptr] = (mem[ptr] + 1) % 256
 
         elif char == BrainfuckKeywords.VAL_DEC:
-            membuf[pointer_idx] = (membuf[pointer_idx] - 1) % 256
+            mem[ptr] = (mem[ptr] - 1) % 256
 
         elif char == BrainfuckKeywords.PTR_INC:
-            pointer_idx += 1
-            if pointer_idx >= memsize:
+            ptr += 1
+            if ptr >= memsize:
                 if wrap:
-                    pointer_idx = 0
+                    ptr = 0
                 else:
                     raise BFSegmentationFault(
                         f"access violation at far-right of memory",
-                        position=instruct_idx, code=code
+                        position=ist, code=code
                     )
 
         elif char == BrainfuckKeywords.PTR_DEC:
-            pointer_idx -= 1
-            if pointer_idx < 0:
+            ptr -= 1
+            if ptr < 0:
                 if wrap:
-                    pointer_idx = memsize - 1
+                    ptr = memsize - 1
                 else:
                     raise BFSegmentationFault(
                         f"access violation at far-left of memory",
-                        position=instruct_idx, code=code
+                        position=ist, code=code
                     )
 
         elif char == BrainfuckKeywords.STDOUT:
-            print(chr(membuf[pointer_idx]), end='', flush=True)
+            print(chr(mem[ptr]), end='', flush=True)
 
         elif char == BrainfuckKeywords.STDIN:
             try:
@@ -73,45 +73,45 @@ def run_brainfuck(code: str, *, memsize: int, wrap: bool = False):
             except KeyboardInterrupt:
                 raise BFInterrupt(
                     "program interrupted by user",
-                    position=instruct_idx, code=code
+                    position=ist, code=code
                 )
             if ch:
-                membuf[pointer_idx] = ord(ch) % 256
+                mem[ptr] = ord(ch) % 256
             else:
-                membuf[pointer_idx] = 0
+                mem[ptr] = 0
 
         elif char == BrainfuckKeywords.LOOP_START:
             # If the cell value is zero, jump forward to matching loop ender
-            if membuf[pointer_idx] == 0:
+            if mem[ptr] == 0:
                 # jump forward to matching ]
                 depth = 1
                 while depth > 0:
-                    instruct_idx += 1
-                    if instruct_idx >= prog_len:
+                    ist += 1
+                    if ist >= prog_len:
                         raise BFSyntaxError(
                             f"unmatched '{BrainfuckKeywords.LOOP_START}'",
-                            position=instruct_idx, code=code
+                            position=ist, code=code
                         )
-                    if code[instruct_idx] == BrainfuckKeywords.LOOP_START:
+                    if code[ist] == BrainfuckKeywords.LOOP_START:
                         depth += 1
-                    elif code[instruct_idx] == BrainfuckKeywords.LOOP_END:
+                    elif code[ist] == BrainfuckKeywords.LOOP_END:
                         depth -= 1
 
         elif char == BrainfuckKeywords.LOOP_END:
             # If cell value is non-zero, jump back to matching loop start
-            if membuf[pointer_idx] != 0:
+            if mem[ptr] != 0:
                 depth = 1
                 while depth > 0:
-                    instruct_idx -= 1
-                    if instruct_idx < 0:
+                    ist -= 1
+                    if ist < 0:
                         raise BFSyntaxError(
                             f"unmatched '{BrainfuckKeywords.LOOP_END}'",
-                            position=instruct_idx, code=code
+                            position=ist, code=code
                         )
-                    if code[instruct_idx] == BrainfuckKeywords.LOOP_END:
+                    if code[ist] == BrainfuckKeywords.LOOP_END:
                         depth += 1
-                    elif code[instruct_idx] == BrainfuckKeywords.LOOP_START:
+                    elif code[ist] == BrainfuckKeywords.LOOP_START:
                         depth -= 1
 
         # Move forward to next instruction
-        instruct_idx += 1
+        ist += 1
